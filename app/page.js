e0a39e9471bfb7ef6286260d8797103b8c82e838
Tushar Chawre <tushar@Tushars-MacBook-Air.local>
2024-03-26 13:34:36 +0530

    Hero Glass

--- app/page.js
+++ app/page.js
@@ -8,27 +8,16 @@ const page = () => {
   return (
     <>
     <div className="main">
-
-   
-
       <div className="glass">
-        
       <div className="tealblob"></div>
       <div className="pinkblob"></div>
       <div className="greenblob"></div>
       <div className="glassblur"></div>
         <div className="noise"></div>
       <div className="hero">
-        <h1>Shit</h1>
+        <h1> <span className="barrier">The</span> Seed <span className="barrier"> of your thoughts when planted at the right time can change</span> Your <span className="barrier">reality, a vivid vision that bloomed into a great</span>  Idea.</h1>
       </div>
       </div>
-
-
-
-
-
-
-
     </div>
   
     </>
